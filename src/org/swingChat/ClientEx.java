63000494d7b980dd424d1728897860069cfa74ea
kimhoyoun <hoyoun1309@gmail.com>
2022-01-18 21:35:29 +0900

    01.18 update

--- src/org/swingChat/ClientEx.java
+++ src/org/swingChat/ClientEx.java
@@ -20,14 +20,13 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ClientEx extends JFrame implements ActionListener {
-	private Socket socket = null;
-	private BufferedReader br = null;
-	private BufferedWriter bw = null;
-	private ChatArea chat = null;
-	private JTextField reader = null;
-	private boolean initflag = false;
-	private String userID = null;
-	private String message = null;
+	private Socket socket = null;		// 서버에 연결할 Socket
+	private BufferedReader br = null;	// 서버가 보낸 메세지를 읽을 입력 스트림
+	private BufferedWriter bw = null;	// 서버로 메세지를 전달할 출력 스트림
+	private ChatArea chat = null;		// 채팅방 (TextArea를 상속받고 쓰레드로 만들기위해 Runnable 인터페이스를 상속받음
+	private JTextField reader = null;	// 메세지를 입력할 곳
+	private boolean initflag = false;	// 클라이언트가 서버에 접속하고 userID를 전달했음을 판단할 boolean 변수
+	private String userID = null;		// userID
 
 	public ClientEx() {
 		setTitle("클라이언트");
@@ -41,19 +40,24 @@ public class ClientEx extends JFrame implements ActionListener {
 		c.add(chat, BorderLayout.CENTER);
 		c.add(reader, BorderLayout.SOUTH);
 		setVisible(true);
+		// 서버에 연결하는 메서드 
 		connectServer();
-
+		
+		// 서버에 연결 후 쓰레드 실행
 		Thread th = new Thread(chat);
 		th.start();
 	}
 
 	public void connectServer() {
 		try {
-			Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
-
+			// localHost ip, 9999 port에 접속
+			socket = new Socket(InetAddress.getLocalHost(), 9999);
+			
+			// 서버에 접속하고 입출력 스트림 생성
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
+			
+			// 서버에 연결하고 userID를 정하기위해 클라이언트에게 알려준다.
 			chat.append("TextField에 userID를 입력해주세요!");
 			
 		} catch (UnknownHostException e) {
@@ -67,11 +71,15 @@ public class ClientEx extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
+		
+		// TextFeild에서 Enter를 누르면 메세지를 전달
 		if (e.getSource() == reader) {
+			// userID를 전달하지 않았다면 첫번째 메세지는 반드시 userID로 서버에 전달하기위한 조건문
 			if (!initflag) {
 				userID = reader.getText();
+				// 이제 채팅을 시작하므로 TextArea 클리어
 				chat.setText("");
+				// TextField에서 메세지 전달받고 클리어
 				reader.setText("");
 				try {
 					bw.write(userID + "\n");
@@ -80,11 +88,14 @@ public class ClientEx extends JFrame implements ActionListener {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
+				// 초기 설정이 완료됨.
 				initflag = true;
 			} else {
+				// TextField에 입력한 메세지를 받아서
 				String msg = reader.getText();
 				reader.setText("");
 				try {
+					// 서버에 메세지 전달 "\"이 빠지면 서버의 InputStream이 읽을 수 없음.
 					bw.write(msg + "\n");
 					bw.flush();
 				} catch (IOException e1) {
@@ -94,13 +105,15 @@ public class ClientEx extends JFrame implements ActionListener {
 			}
 		}
 	}
-
+	
+	// 쓰레드로 동작하는 TextArea
 	class ChatArea extends JTextArea implements Runnable {
 		@Override
 		public void run() {
 			while(true) {
 				
 			try {
+				// 서버로부터 메세지를 전달받으면 TextArea에 보여줌.
 				String msg = br.readLine();
 				chat.append(msg +"\n");
 			} catch (IOException e) {
--- src/org/swingChat/ServerEx.java
+++ src/org/swingChat/ServerEx.java
@@ -21,10 +21,9 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ServerEx extends JFrame{
-	static HashMap hm = new HashMap();
-	private JTextArea chat = null;
-	private JTextField writer = null;
-	private ConnectClient cc = null;
+	static HashMap hm = new HashMap();	// userID와 BufferdWriter를 key-value 쌍으로 저장하여 상황에 따라 메세지를 보내줄 수 있도록 함.
+	private JTextArea chat = null; 		// 서버의 상황과 메세지를 입력받았을 때 상황을 보여주기 위한 TextArea
+	private ConnectClient cc = null;	// 서버가 멈추지않고 동작하기위해 Thread로 만들어줌.
 
 	public ServerEx() {
 		setTitle("서버");
@@ -33,10 +32,8 @@ public class ServerEx extends JFrame{
 
 		Container c = getContentPane();
 		chat = new JTextArea();
-		writer = new JTextField();
 		c.setLayout(new BorderLayout());
 		c.add(new JScrollPane(chat), BorderLayout.CENTER);
-		c.add(writer, BorderLayout.SOUTH);
 		setVisible(true);
 
 		// 서버 쓰레드 동작
@@ -48,7 +45,8 @@ public class ServerEx extends JFrame{
 	public static void main(String[] args) {
 		new ServerEx();
 	}
-
+	
+	// 클라이언트 접속을 대기하는 쓰레드
 	class ConnectClient extends Thread {
 		private ServerSocket listener = null;
 		private Socket socket = null;
@@ -56,12 +54,15 @@ public class ServerEx extends JFrame{
 		@Override
 		public void run() {
 			try {
+				// 서버는 한번만 생성
 				listener = new ServerSocket(9999);
 				chat.append("접속대기중...\n");
 				while (true) {
+					// 접속은 계속 유지해야되므로 wile문 안에서 무한반복
 					socket = listener.accept();
 					chat.append("클라이언트 접속 완료...\n");
-
+					
+					// 클라이언트가 서버에 접속 시 해당 클라이언트와 메세지를 주고 받을 쓰레드 생성
 					ServerThread sth = new ServerThread(socket);
 					sth.start();
 				}
@@ -78,17 +79,23 @@ public class ServerEx extends JFrame{
 		private BufferedReader br = null;
 		private BufferedWriter bw = null;
 		private String userID;
-
+		
+		// 생성자에 socket을 받아 서버에 접속한 클라이언트의 socket정보를 받아옴.
 		public ServerThread(Socket socket) {
 			this.socket = socket;
 			try {
+				// 클라이언트와 메세지를 주고받기위해 입출력 스트림 생성
 				br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
+				
+				// 클라이언트의 첫번째 입력은 userID로 정의했기 때문에 첫번째 입력을 userID 변수에 저장.
 				userID = br.readLine();
 				String msg = userID + " 접속하였습니다!";
+				// 클라이언트로부터 입력받은 메세지를 서버의 TextArea에 출력
 				chat.append(msg + "\n");
+				// 다대다 통신을 위해 HashMap에 userID와 BufferedWriter를 저장해준다.
 				hm.put(userID, bw);
+				// 클라이언트 접속 정보를 다른 클라이언트에게 전송하기위한 broadCast()메서드 호출
 				broadCast(msg);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -96,18 +103,24 @@ public class ServerEx extends JFrame{
 			}
 
 		}
-
+		
+		// 쓰레드가 시작되면 반복할 코드
 		@Override
 		public void run() {
 			while (true) {
 				try {
+					// 클라이언트로부터 메세지를 받을때까지 대기하면 받으면 msg 변수에 저장
 					String msg = br.readLine();
-
+					
+					// 메세지에 /to라는 문구가 있고 그 문구가 메세지의 제일 처음에 있다면 directMessage를 보냄.
 					if ((msg.indexOf("/to") != -1) && (msg.indexOf("/to") == 0)) {
 						directMessage(msg);
 					} else {
+						// 어떤 클라이언트가 보낸 메세지인지 알 수 있도록 메세지앞에 userID를 붙여준다.
 						msg = userID + " : " + msg;
+						// 서버창에도 메세지를 보이게해줌
 						chat.append(msg + "\n");
+						// DM이 아니라면 전체 클라이언트에게 메세지를 보냄
 						broadCast(msg);
 
 					}
@@ -119,13 +132,18 @@ public class ServerEx extends JFrame{
 
 			}
 		}
-
+		
+		// 전체 클라이언트에게 메세지 보내는 메서드
 		public void broadCast(String msg) {
+			// hashmap의 value를 collection에 저장(Iterator를 사용하기 위함)
 			Collection collection = hm.values();
+			// Iterator로 만들어서 모든 클라이언트의 BufferedWriter를 가져온다.
 			Iterator it = collection.iterator();
 			while (it.hasNext()) {
+				// Iterator의 제네릭에 타입을 안넣어주었으므로 Iterator가 갖고있는 객체(Object type)를 BW타입으로 강제 형변환 해준다.
 				BufferedWriter print = (BufferedWriter) it.next();
 				try {
+					// print는 반복이 되면서 각 클라이언트의 Socket에 메세지를 보낸다.
 					print.write(msg + "\n");
 					print.flush();
 				} catch (IOException e) {
@@ -135,26 +153,39 @@ public class ServerEx extends JFrame{
 
 		public void directMessage(String msg) {
 			int start, end;
+			// 클라이언트가 DM을 발송하면 /to userName 메세지 형식이므로 첫번째 공백을 찾는다.
 			start = msg.indexOf(" ");
+			// 첫번째 공백 이후로 검사하여 두번째 공백을 찾는다.
 			end = msg.indexOf(" ", start + 1);
-
+			
+			// 공백을 통해 전달해야할 클라이언트의 userID를 찾음.
 			String user = msg.substring(start + 1, end);
-
+			
+			// 메세지 내용만 추출
 			String message = msg.substring(end + 1);
+			// 누가 누구한테 보내는지 명확하게 하기위해 format을 지정하고 서버 TextArea에 띄움.
 			chat.append(userID + " -> " + user + " : " + message + "\n");
 			
+			
 			if (hm.get(user) != null) {
+				// 누가 보낸 메세지인지 알려주기위해 보낸 클라이언트의 메세지에 userID 추가
 				message = userID + " : " + message;
+				// 모두가 아닌 특정 클라이언트에게 보내기위해 HashMap.get()사용해서
+				// 전달해줄 userID를 갖는 클라이언트의 BufferedWriter를 찾음.
 				BufferedWriter print = (BufferedWriter) hm.get(user);
 				try {
+					// 메세지를 보낸 클라이언트도 메세지를 확인할 수 있도록 보낸 클라이언트에게도 메세지 전송
+					// 메세지를 보낸 클라이언트의 BW는 현재 갖고있으므로 찾을 필요가 없다.
 					bw.write(message + "\n");
 					bw.flush();
+					// 메세지를 받을 클라이언트에게만 메세지를 전달.
 					print.write(message + "\n");
 					print.flush();
 				} catch (IOException e) {
 
 				}
 
+			// userID가 일치하는 클라이언트가 없다면 메세지를 보낸 클라이언트에게 해당 클라이언트를 찾을 수 없다는 메세지 전송
 			}else {
 				message = "not found "+user;
 				chat.append(message + "\n");
